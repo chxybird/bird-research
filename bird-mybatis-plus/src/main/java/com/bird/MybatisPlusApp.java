8dd4314a2e0f27ee6e7972aa382e35204d22e560
bird <2450107493@qq.com>
2021-04-06 14:57:11 +0800

    调研提交

---
+++ bird-mybatis-plus/src/main/java/com/bird/MybatisPlusApp.java
@@ -0,0 +1,16 @@
+package com.bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lipu
+ * @Date 2021/4/6 14:55
+ * @Description
+ */
+@SpringBootApplication
+public class MybatisPlusApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApp.class);
+    }
+}
---
+++ bird-mybatis-plus/src/test/java/MybatisLambdaTest.java
@@ -0,0 +1,15 @@
+import com.bird.MybatisPlusApp;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author lipu
+ * @Date 2021/4/6 14:54
+ * @Description
+ */
+@SpringBootTest(classes = MybatisPlusApp.class)
+@RunWith(SpringRunner.class)
+public class MybatisLambdaTest {
+
+}
