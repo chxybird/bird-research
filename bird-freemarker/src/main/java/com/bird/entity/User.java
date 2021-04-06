9c0a2761f9b6462d94af6d96e4c9011baacc0774
lipu <2450107493@qq.com>
2021-04-06 13:33:46 +0800

    调研提交

---
+++ bird-freemarker/src/main/java/com/bird/entity/User.java
@@ -0,0 +1,15 @@
+package com.bird.entity;
+
+import lombok.Data;
+
+/**
+ * @Author lipu
+ * @Date 2021/04/01 15:31
+ * @Description
+ */
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+}
